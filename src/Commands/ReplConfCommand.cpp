f5edd099e2b8a7f70031e8890a6d3098fd01aee3
casio15 <tomer1452002@gmail.com>
2024-03-10 19:38:03 +0200

    pass stage 8 repl

---
+++ src/Commands/ReplConfCommand.cpp
@@ -0,0 +1,18 @@
+#include "ReplConfCommand.hpp"
+
+#include "../KeyValueStore.hpp"
+#include "../RedisServer.hpp"
+
+#include <algorithm>
+
+void ReplConfCommand::execute(int clientFd,
+                              const std::vector<std::string>& command,
+                              RedisServer* serverInstance)
+{
+    auto response = ResponseBuilder::ok();
+
+    if (send(clientFd, response.data(), response.length(), 0) < 0)
+    {
+        std::cerr << "Could not send SET response to client\n";
+    }
+}
